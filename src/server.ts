084d7a2d26d8a2e4a162c882b31e7064b93d466d
Krishna-69 <krishnarastogi67@gmail.com>
2026-07-17 22:09:16 +0530

    Dotenv added

--- src/server.ts
+++ src/server.ts
@@ -1,159 +1,191 @@
-import { WebSocketServer, WebSocket } from "ws"
+import { WebSocketServer, WebSocket } from "ws";
 import { createServer } from "http";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
+const port = process.env.PORT || 8080;
 
 interface Client {
-    room: string;
-    name: string;
+  room: string;
+  name: string;
 }
 
 const clients = new Map<WebSocket, Client>();
-const rooms = new Map<string, Set<WebSocket>>()
+const rooms = new Map<string, Set<WebSocket>>();
 
 function generateRoomCode(): string {
-    return Math.random().toString(36).substring(2, 8).toUpperCase();
+  return Math.random().toString(36).substring(2, 8).toUpperCase();
 }
 
 function getRoomUserCount(room: string): number {
-    return rooms.get(room)?.size ?? 0;
+  return rooms.get(room)?.size ?? 0;
 }
 
 function broadCastToRoom(room: string, message: object, exclude?: WebSocket) {
-    const roomClients = rooms.get(room);
-    if (!roomClients) {
-        return;
-    }
+  const roomClients = rooms.get(room);
+  if (!roomClients) {
+    return;
+  }
 
-    for (const clientSocket of roomClients) {
-        if (clientSocket !== exclude && clientSocket.readyState === WebSocket.OPEN) {
-            clientSocket.send(JSON.stringify(message));
-        }
+  for (const clientSocket of roomClients) {
+    if (
+      clientSocket !== exclude &&
+      clientSocket.readyState === WebSocket.OPEN
+    ) {
+      clientSocket.send(JSON.stringify(message));
     }
+  }
 }
 
 function broadCastUserCount(room: string) {
-    const count = getRoomUserCount(room);
-    const roomClients = rooms.get(room);
-    
-    if (!roomClients) return;
+  const count = getRoomUserCount(room);
+  const roomClients = rooms.get(room);
 
-    for (const clientSocket of roomClients) {
-        if (clientSocket.readyState === WebSocket.OPEN) {
-            clientSocket.send(JSON.stringify({ 
-                type: "UserCount", 
-                count
-            }));
-        }
+  if (!roomClients) return;
+
+  for (const clientSocket of roomClients) {
+    if (clientSocket.readyState === WebSocket.OPEN) {
+      clientSocket.send(
+        JSON.stringify({
+          type: "UserCount",
+          count,
+        }),
+      );
     }
+  }
 }
 
 app.get("/health", (req, res) => {
-    res.json({ status: "ok", activeRooms: rooms.size });
-})
+  res.json({ status: "ok", activeRooms: rooms.size });
+});
 
 app.post("/room/create", (req, res) => {
-    const code = generateRoomCode();
-    rooms.set(code, new Set());
+  const code = generateRoomCode();
+  rooms.set(code, new Set());
 
-    // Auto-delete if nobody joins in 5 minutes
-    setTimeout(() => {
+  // Auto-delete if nobody joins in 5 minutes
+  setTimeout(
+    () => {
+      const room = rooms.get(code);
+
+      if (room && room.size === 0) {
         rooms.delete(code);
-        console.log(`Room ${code} expired (nobody joined)`);
-    }, 5 * 60 * 1000);
+        console.log(`Room ${code} expired`);
+      }
+    },
+    5 * 60 * 1000,
+  );
 
-    console.log(`Room creaded: ${code}`);
-    res.json({ roomCode: code });
+  console.log(`Room creaded: ${code}`);
+  res.json({ roomCode: code });
 });
 
 app.get("/room/:code", (req, res) => {
-    const code = req.params.code;
-    const exists = rooms.has(code);
-    const UserCount = getRoomUserCount(code);
-    res.json({ exists, UserCount });
+  const code = req.params.code;
+  const exists = rooms.has(code);
+  const UserCount = getRoomUserCount(code);
+  res.json({ exists, UserCount });
 });
-
 
 const server = createServer(app);
 const wss = new WebSocketServer({ server });
 
 wss.on("connection", (socket) => {
-    console.log("New Client Connected");
-    
-    socket.on("message", (data) => {
-        try {
-            const parsedData = JSON.parse(data.toString());
+  console.log("New Client Connected");
 
-            if (parsedData.type === "join") {
-                const room: string = parsedData.room.toUpperCase();
-                const name: string = parsedData.name ?? "Anonymous";
-                
-                clients.set(socket, { room, name });
+  socket.on("message", (data) => {
+    try {
+      const parsedData = JSON.parse(data.toString());
 
-                if (!rooms.has(room)) rooms.set(room, new Set());
-                rooms.get(room)!.add(socket);
-                    console.log(`${name} joined room: ${room}`);
-                    
-                socket.send(JSON.stringify({
-                    type: "joined",
-                    room,
-                    message: `You joined room ${room}`,
-                    userCount: getRoomUserCount(room),
-                }));
+      if (parsedData.type === "join") {
+        const room: string = parsedData.room.toUpperCase();
+        const name: string = parsedData.name ?? "Anonymous";
 
-                broadCastUserCount(room);
-                broadCastToRoom(room, {
-                    type: 'system',
-                    message: `${name} joined the room`
-                }, socket);
+        clients.set(socket, { room, name });
 
-                return;
-            }
+        if (!rooms.has(room)) rooms.set(room, new Set());
+        rooms.get(room)!.add(socket);
+        console.log(`${name} joined room: ${room}`);
 
-            if (parsedData.type === "chat") {
-                const client = clients.get(socket);
-                if (!client) return;
+        socket.send(
+          JSON.stringify({
+            type: "joined",
+            room,
+            message: `You joined room ${room}`,
+            userCount: getRoomUserCount(room),
+          }),
+        );
 
-                broadCastToRoom(client.room, {
-                    type: "chat",
-                    message: parsedData.message,
-                    sender: client.name
-                }, socket);
-            }
-        } catch (err) {
-            console.log("Failed to parse message:", err);
-            socket.send(JSON.stringify({ type: "error", message: "Invalid message format" }));
-        }
-    });
+        broadCastUserCount(room);
+        broadCastToRoom(
+          room,
+          {
+            type: "system",
+            message: `${name} joined the room`,
+          },
+          socket,
+        );
 
-    socket.on("close", () => {
+        return;
+      }
+
+      if (parsedData.type === "chat") {
         const client = clients.get(socket);
-        if (client) {
-            const { room, name } = client;
-            rooms.get(room)?.delete(socket);
+        if (!client) return;
 
-            if (rooms.get(room)?.size === 0) {
-                rooms.delete(room);
-                console.log(`Room ${room} deleted (empty)`);;
-            } else {
-                broadCastUserCount(room);
-                broadCastToRoom(room, {
-                    type: "system",
-                    message: `${name} left the room`
-                })
-            }
-            clients.delete(socket);
-        }
-    });
+        broadCastToRoom(
+          client.room,
+          {
+            type: "chat",
+            message: parsedData.message,
+            sender: client.name,
+          },
+          socket,
+        );
+      }
+    } catch (err) {
+      console.log("Failed to parse message:", err);
+      socket.send(
+        JSON.stringify({ type: "error", message: "Invalid message format" }),
+      );
+    }
+  });
 
-    socket.on("error", (err) => console.log("WebSocket Error:", err))
+  socket.on("close", () => {
+    const client = clients.get(socket);
+    if (client) {
+      const { room, name } = client;
+      rooms.get(room)?.delete(socket);
+
+      if (rooms.get(room)?.size === 0) {
+        rooms.delete(room);
+        console.log(`Room ${room} deleted (empty)`);
+      } else {
+        broadCastUserCount(room);
+        broadCastToRoom(room, {
+          type: "system",
+          message: `${name} left the room`,
+        });
+      }
+      clients.delete(socket);
+    }
+  });
+
+  socket.on("error", (err) => console.log("WebSocket Error:", err));
 });
 
-server.listen(8080, () => {
-    console.log("HTTP -> http://localhost:8080");
-    console.log("WS -> http://localhost:8080");
+server.listen(port, () => {
+  console.log(`HTTP -> http://localhost:${port}`);
+  console.log(`WS -> http://localhost:${port}`);
 });
